b4ce4665df067db65de93899549f559b908ffdb1
khabir-salah <sabdulkabir19@gmail.com>
2025-05-23 18:49:50 +0100

    Db migrations added

--- Infrastructure/Persistence/Context/ApplicationDbContext.cs
+++ Infrastructure/Persistence/Context/ApplicationDbContext.cs
@@ -3,6 +3,8 @@ using Domain.Aggregate.TournamentAggregate;
 using Domain.Entities;
 using Domain.ValueObject;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 
 namespace Infrastructure.Persistence.Context;
 
@@ -31,4 +33,35 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
         modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
     }
     
+}
+
+public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+        // Load config from appsettings.json or environment
+
+        var relativePathToApi = Path.Combine("..", "api"); // Adjust as per your actual structure
+        Console.WriteLine($"Relative path segment to API: {relativePathToApi}");
+
+
+        var apiProjectDirectory = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "..", "api"));
+
+        Console.WriteLine($"Calculated API project directory: {apiProjectDirectory}");
+
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(apiProjectDirectory) // Set the base path to where appsettings.json actually resides
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true) // Good practice for dev settings
+            .Build();
+
+
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        optionsBuilder.UseMySQL(connectionString);
+
+        return new ApplicationDbContext(optionsBuilder.Options);
+    }
 }
